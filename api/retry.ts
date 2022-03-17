a6222937d26ce75d2d7ee3b3a13f0f3fde66ecfc
Tim Smart <tim.smart@arisechurch.com>
2022-03-17 21:54:46 +1300

    fix: widen retry types

--- api/retry.ts
+++ api/retry.ts
@@ -7,36 +7,39 @@ import { catchError_ } from "./catchError"
 import { tapEffect } from "./tapEffect"
 import { unwrap } from "./unwrap"
 
-export const retry_ = <R, E, A, Z>(
+export const retry_ = <R, R1, E, A, Z>(
   self: EffectSource<R, E, A>,
-  schedule: SC.Schedule<R, E, Z>,
-): EffectSource<R & HasClock, E, A> =>
+  schedule: SC.Schedule<R1, E, Z>,
+): EffectSource<R & R1 & HasClock, E, A> =>
   pipe(
     SC.driver(schedule),
     T.map((driver) => {
-      const loop: EffectSource<R & HasClock, E, A> = catchError_(self, (e) =>
-        pipe(
-          driver.next(e),
-          T.foldM(
-            () => T.succeed(loop),
-            () =>
-              T.succeed(
-                pipe(
-                  loop,
-                  tapEffect(() => driver.reset),
+      const loop: EffectSource<R & R1 & HasClock, E, A> = catchError_(
+        self,
+        (e) =>
+          pipe(
+            driver.next(e),
+            T.foldM(
+              () => T.succeed(loop),
+              () =>
+                T.succeed(
+                  pipe(
+                    loop,
+                    tapEffect(() => driver.reset),
+                  ),
                 ),
-              ),
+            ),
+            unwrap,
           ),
-          unwrap,
-        ),
       )
 
       return loop
     }),
     unwrap,
   )
+SC.delayed
 
 export const retry =
-  <R, E, Z>(schedule: SC.Schedule<R, E, Z>) =>
-  <A>(self: EffectSource<R, E, A>): EffectSource<R & HasClock, E, A> =>
+  <R1, E, Z>(schedule: SC.Schedule<R1, E, Z>) =>
+  <R, A>(self: EffectSource<R, E, A>): EffectSource<R & R1 & HasClock, E, A> =>
     retry_(self, schedule)
