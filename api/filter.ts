f94d1abff8c23a92e24ce10f98a902bfe3f1f85e
Tim Smart <tim.smart@arisechurch.com>
2022-03-18 12:17:29 +1300

    fix: allow boolean predicates in filter

--- api/filter.ts
+++ api/filter.ts
@@ -2,18 +2,34 @@
 import cbFilter from "callbag-filter"
 import { EffectSource } from "../types"
 
-export const filter_ =
-  <R, E, A, B extends A>(
-    self: EffectSource<R, E, A>,
-    pred: (a: A) => a is B,
-  ): EffectSource<R, E, B> =>
-  (r) =>
-    cbFilter(pred)(self(r) as any) as any
+export function filter_<R, E, A, B extends A>(
+  self: EffectSource<R, E, A>,
+  pred: (a: A) => a is B,
+): EffectSource<R, E, B>
+
+export function filter_<R, E, A>(
+  self: EffectSource<R, E, A>,
+  pred: (a: A) => boolean,
+): EffectSource<R, E, A>
+
+export function filter_<R, E, A>(
+  self: EffectSource<R, E, A>,
+  pred: (a: A) => boolean,
+): EffectSource<R, E, A> {
+  return (r) => cbFilter(pred)(self(r) as any) as any
+}
 
 /**
  * @ets_data_first filter_
  */
-export const filter =
-  <A, B extends A>(fab: (a: A) => a is B) =>
-  <R, E>(source: EffectSource<R, E, A>) =>
-    filter_(source, fab)
+export function filter<A, B extends A>(
+  fab: (a: A) => a is B,
+): <R, E>(source: EffectSource<R, E, A>) => EffectSource<R, E, B>
+
+export function filter<A>(
+  pred: (a: A) => boolean,
+): <R, E>(self: EffectSource<R, E, A>) => EffectSource<R, E, A>
+
+export function filter<A>(pred: (a: A) => boolean) {
+  return <R, E>(self: EffectSource<R, E, A>) => filter_(self, pred)
+}
