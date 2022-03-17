d6198ee4e31d4dec166ee48ee8f87f8284e4bd42
Tim Smart <tim.smart@arisechurch.com>
2022-03-17 20:36:55 +1300

    fix: only replace source once in catchError

--- api/catchError.ts
+++ api/catchError.ts
@@ -1,14 +1,19 @@
 import { EffectSource, Signal, Talkback } from "../types"
 
 export const catchError_ =
-  <R, E, A>(onError: (e: E) => EffectSource<R, E, A>) =>
-  (fa: EffectSource<R, E, A>): EffectSource<R, E, A> =>
+  <R, R1, E, E1, A, B>(
+    self: EffectSource<R, E, A>,
+    onError: (e: E) => EffectSource<R1, E1, B>,
+  ): EffectSource<R & R1, E | E1, A | B> =>
   (r) =>
   (_, sink) => {
     let innerTalkback: Talkback
     const talkback: Talkback = (signal) => innerTalkback?.(signal)
 
-    const replaceSource = (fa: EffectSource<R, E, A>, initial = false) => {
+    const replaceSource = (
+      fa: EffectSource<R & R1, E | E1, A | B>,
+      initial = false,
+    ) => {
       fa(r)(Signal.START, (t, d) => {
         if (t === Signal.START) {
           innerTalkback = d
@@ -18,13 +23,23 @@ export const catchError_ =
           } else {
             talkback(Signal.DATA)
           }
-        } else if (t === Signal.END && d !== undefined && d._tag === "Fail") {
-          replaceSource(onError(d.value))
+        } else if (
+          initial &&
+          t === Signal.END &&
+          d !== undefined &&
+          d._tag === "Fail"
+        ) {
+          replaceSource(onError(d.value as E))
         } else {
           sink(t as any, d as any)
         }
       })
     }
 
-    replaceSource(fa, true)
+    replaceSource(self, true)
   }
+
+export const catchError =
+  <R1, E, E1, B>(onError: (e: E) => EffectSource<R1, E1, B>) =>
+  <R, A>(fa: EffectSource<R, E, A>): EffectSource<R & R1, E | E1, A | B> =>
+    catchError_(fa, onError)
