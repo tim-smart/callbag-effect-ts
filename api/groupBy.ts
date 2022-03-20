d545cef31b00bd82e9b67c3fe5e4201bf26a122f
Tim Smart <tim.smart@arisechurch.com>
2022-03-20 19:15:14 +1300

    feat: groupBy operator

---
+++ api/groupBy.ts
@@ -0,0 +1,24 @@
+import { EffectSource } from "../types"
+import * as GB from "./_internal/groupBy"
+import { map } from "./map"
+import { pipe } from "@effect-ts/core"
+import { CustomRuntime } from "@effect-ts/core/Effect"
+
+export const groupBy_ = <R, E, A, K>(
+  self: EffectSource<R, E, A>,
+  keyFn: (a: A) => K,
+): EffectSource<R, E, readonly [source: EffectSource<R, E, A>, key: K]> =>
+  pipe(
+    (r: CustomRuntime<R, unknown>) => GB.groupBy_(self(r), keyFn),
+    map(([source, key]) => [(_) => source, key] as const),
+  )
+
+/**
+ * @ets_data_first groupBy_
+ */
+export const groupBy =
+  <A, K>(keyFn: (a: A) => K) =>
+  <R, E>(
+    self: EffectSource<R, E, A>,
+  ): EffectSource<R, E, readonly [source: EffectSource<R, E, A>, key: K]> =>
+    groupBy_(self, keyFn)
