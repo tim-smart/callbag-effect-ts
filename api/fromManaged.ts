6e7c3c54bcd16b007ce6d5f729f3c6e4959c5a70
Tim Smart <tim.smart@arisechurch.com>
2022-04-01 22:59:30 +1300

    refactor: fromManaged doesn't end

--- api/fromManaged.ts
+++ api/fromManaged.ts
@@ -1,9 +1,40 @@
 // ets_tracing: off
 import * as T from "@effect-ts/core/Effect"
+import { AsyncCancel } from "@effect-ts/core/Effect"
 import * as M from "@effect-ts/core/Effect/Managed"
+import { Signal } from "strict-callbag-basics"
 import { EffectSource } from "../types"
-import { fromEffect } from "./fromEffect"
 
-export const fromManaged = <R, E, A>(
-  fa: M.Managed<R, E, A>,
-): EffectSource<R, E, A> => fromEffect(M.use_(fa, T.succeed))
+export const fromManaged =
+  <R, E, A>(fa: M.Managed<R, E, A>): EffectSource<R, E, A> =>
+  (r) =>
+  (_, sink) => {
+    let aborted = false
+    let cancel: AsyncCancel<E, void>
+
+    sink(Signal.START, (signal) => {
+      if (signal === Signal.DATA) {
+        cancel ??= r.runCancel(
+          M.use_(fa, (a) => {
+            if (aborted) return T.unit
+            sink(Signal.DATA, a)
+            return T.never
+          }),
+          (e) => {
+            if (aborted) return
+
+            if (e._tag === "Success") {
+              sink(Signal.END, undefined)
+            } else {
+              sink(Signal.END, e.cause)
+            }
+          },
+        )
+      } else if (signal === Signal.END) {
+        aborted = true
+        if (cancel) {
+          r.run(cancel)
+        }
+      }
+    })
+  }
