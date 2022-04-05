acb2f6776d318c2311b3694a7bd433a9e7421959
Tim Smart <tim.smart@arisechurch.com>
2022-04-05 12:32:42 +1200

    fix: noop sink

--- Sink/noop.ts
+++ Sink/noop.ts
@@ -2,4 +2,4 @@ import * as CBS from "strict-callbag-basics/Sink"
 import { EffectSink } from "../types"
 
 export const noop: EffectSink<unknown, unknown, never, unknown> = (_) =>
-  CBS.noop
+  CBS.noop()
--- api/forEach.ts
+++ api/forEach.ts
@@ -1,13 +1,13 @@
 // ets_tracing: off
 import * as T from "@effect-ts/core/Effect"
+import * as FE from "../Sink/forEach"
 import { EffectSource } from "../types"
-import { tap_ } from "./tap"
-import { runDrain } from "./run"
+import { run_ } from "./run"
 
 export const forEach_ = <R, R1, E, E1, A>(
   self: EffectSource<R, E, A>,
   f: (a: A) => T.Effect<R1, E1, any>,
-) => runDrain(tap_(self, f))
+) => run_(self, FE.forEach(f))
 
 /**
  * @ets_data_first forEach_
@@ -15,4 +15,4 @@ export const forEach_ = <R, R1, E, E1, A>(
 export const forEach =
   <R1, E1, A>(f: (a: A) => T.Effect<R1, E1, any>) =>
   <R, E>(self: EffectSource<R, E, A>) =>
-    runDrain(tap_(self, f))
+    forEach_(self, f)
