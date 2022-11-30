df791fb9454b4bd0c86b9a1188d318cc8a15ceea
Tim Smart <tim.smart@arisechurch.com>
2022-11-30 15:33:20 +1300

    refactor: remove requirements from fromSchedule

--- src/Source/fromSchedule.ts
+++ src/Source/fromSchedule.ts
@@ -1,14 +1,10 @@
-import { Clock } from "@effect/io/Clock"
 import * as T from "@effect/io/Effect"
 import * as SC from "@effect/io/Schedule"
 import { pipe } from "callbag-effect-ts/Source"
-import { EffectSource } from "../Source.js"
 import { repeatEffectOption } from "./repeatEffectOption.js"
 import { unwrap } from "./unwrap.js"
 
-export const fromSchedule = <R, A>(
-  schedule: SC.Schedule<R, unknown, A>,
-): EffectSource<R | Clock, never, A> =>
+export const fromSchedule = <R, A>(schedule: SC.Schedule<R, unknown, A>) =>
   pipe(
     SC.driver(schedule),
     T.map((driver) => repeatEffectOption(driver.next(undefined))),
