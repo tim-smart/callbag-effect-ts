58b445b79cf0c530818a47641c0dc48bf06b9b15
Tim Smart <tim.smart@arisechurch.com>
2022-12-01 21:17:32 +1300

    refactor!: clean up share to return effect

--- src/Source/share.ts
+++ src/Source/share.ts
@@ -1,33 +1,15 @@
-import { Cause } from "@effect/io/Cause"
 import * as T from "@effect/io/Effect"
-import * as Scope from "@effect/io/Scope"
-import * as CB from "strict-callbag-basics"
 import { pipe } from "callbag-effect-ts/Source"
+import * as CB from "strict-callbag-basics"
 import { EffectSource } from "../Source.js"
-import { unwrap } from "./unwrap.js"
 
-export const share = <R, E, A>(self: EffectSource<R, E, A>) => {
-  const cache = new Map<Scope.Scope, CB.Source<A, Cause<E>>>()
-
-  const make = pipe(
-    T.struct({
-      runtime: T.runtime<R>(),
-      scope: T.service(Scope.Tag),
-    }),
-    T.map(({ runtime, scope }) => {
-      if (cache.has(scope)) {
-        return cache.get(scope)!
-      }
-      const source = CB.share(self(runtime))
-      cache.set(scope, source)
-      return source
-    }),
+export const share = <R, E, A>(self: EffectSource<R, E, A>) =>
+  pipe(
+    T.runtime<R>(),
+    T.map((runtime) => CB.share(self(runtime))),
     T.map(
-      (source): EffectSource<R, E, A> =>
+      (source): EffectSource<never, E, A> =>
         (_) =>
           source,
     ),
   )
-
-  return unwrap(make)
-}
