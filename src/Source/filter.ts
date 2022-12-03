031e77422857e90185bb959dda452f1bbbe70bb6
Tim Smart <tim.smart@arisechurch.com>
2022-12-03 20:01:30 +1300

    refactor: inline filter overloads

--- src/Source/filter.ts
+++ src/Source/filter.ts
@@ -1,16 +1,15 @@
 import * as CB from "strict-callbag-basics"
 import { EffectSource } from "../Source.js"
 
-export function filter<A, B extends A>(
-  fab: (a: A) => a is B,
-): <R, E>(source: EffectSource<R, E, A>) => EffectSource<R, E, B>
-
-export function filter<A>(
-  pred: (a: A) => boolean,
-): <R, E>(self: EffectSource<R, E, A>) => EffectSource<R, E, A>
-
-export function filter<A>(pred: (a: A) => boolean) {
-  return <R, E>(self: EffectSource<R, E, A>): EffectSource<R, E, A> =>
-    (r) =>
-      CB.filter_(self(r), pred)
-}
+export const filter: {
+  <A, B extends A>(fab: (a: A) => a is B): <R, E>(
+    self: EffectSource<R, E, A>,
+  ) => EffectSource<R, E, B>
+  <A>(fab: (a: A) => boolean): <R, E>(
+    self: EffectSource<R, E, A>,
+  ) => EffectSource<R, E, A>
+} =
+  <A>(pred: (a: A) => boolean) =>
+  <R, E>(self: EffectSource<R, E, A>): EffectSource<R, E, A> =>
+  (r) =>
+    CB.filter_(self(r), pred)
